c134d7124947061b25ae9aea10d983a1931e6943
Vansh Pratap Singh Chauhan <chauhanvanshpratapsingh@gmail.com>
2026-09-13 17:44:51 +0530

    static method question

---
+++ pr17.cpp
@@ -0,0 +1,18 @@
+// #include<iostream>
+// using namespace std;
+
+// void attendence()
+// {
+//     static int count = 0;
+//     count++;
+//     cout<<"attendence is :"<<count<<endl;
+// }
+
+// int main()
+// {
+//     attendence();
+//     attendence();
+//     attendence();   
+//     return 0;
+// }
+
